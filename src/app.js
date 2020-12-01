adbb8e88e45d9d82fbaf54f855f5288bdaf3d072
dai-yu <1435480344@qq.com>
2020-12-01 13:40:04 +0800

    install sass

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import React from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
+import './index.scss'
 
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-);
+ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
